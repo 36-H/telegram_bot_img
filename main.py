3838d1cefed900bfeaa838001cbf64b2154a83e7
Helios Qiu <admin@lovevup.com>
2025-03-24 23:41:03 +0800

    feat:支持以文件形式的图片上传并优化消息格式

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
-
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters, CallbackContext
 
 # 加载 .env 文件
 load_dotenv()
@@ -54,9 +53,7 @@ def upload(photo_path):
             raise Exception(f"上传失败: {photo_path} - 状态码: {response.status_code}, 响应: {response.text}")
 
 
-async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    photo_file = await context.bot.get_file(update.message.photo[-1].file_id)
-
+async def download_and_upload(update: Update,photo_file) -> None:
     # 保存文件路径
     photo_path = os.path.basename(photo_file.file_path)
     await photo_file.download_to_drive(photo_path)
@@ -71,13 +68,12 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
             webp = urls['webp']
             markdown_link = f"![image]({URL}/{webp})"
 
-            await update.message.reply_text(
-                f"<b>上传成功！<b>\n\n"
-                f"<b>原始图片链接</b>\n\n<pre>{URL}/{original}</pre>\n\n"
-                f"<b>AVIF图片链接</b>\n\n<pre>{URL}/{avif}</pre>\n\n"
-                f"<b>WebP图片链接</b>\n\n<pre>{URL}/{webp}</pre>\n\n"
-                f"<b>Markdown</b>\n\n<pre>{markdown_link}</pre>\n\n",
-                parse_mode='Markdown'
+            await update.message.reply_html(
+                f"<b>上传成功！</b>\n\n"
+                f"<b>原始图片链接</b><pre>{URL}/{original}</pre>\n\n"
+                f"<b>AVIF图片链接</b><pre>{URL}/{avif}</pre>\n\n"
+                f"<b>WebP图片链接</b><pre>{URL}/{webp}</pre>\n\n"
+                f"<b>Markdown</b><pre>{markdown_link}</pre>\n\n",
             )
         else:
             await update.message.reply_text(f"图片上传失败：{result['status']}")
@@ -88,10 +84,17 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         if os.path.exists(photo_path):
             os.remove(photo_path)
 
+async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    photo_file = await context.bot.get_file(update.message.photo[-1].file_id)
+    await download_and_upload(update, photo_file)
+
 
 async def handle_non_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.delete()
 
+async def handle_document_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    photo_file = await context.bot.get_file(update.message.document.file_id)
+    await download_and_upload(update, photo_file)
 
 def main():
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
@@ -99,7 +102,8 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.PHOTO, handle_photo))
-    application.add_handler(MessageHandler(~filters.PHOTO & ~filters.COMMAND, handle_non_photo))
+    application.add_handler(MessageHandler(filters.Document.IMAGE, handle_document_image))
+    application.add_handler(MessageHandler(~filters.PHOTO & ~filters.COMMAND & filters.Document.IMAGE, handle_non_photo))
 
     application.run_polling()
 
